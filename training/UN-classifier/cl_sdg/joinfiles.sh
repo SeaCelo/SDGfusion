542cd2066af5519b9a20e21ad88ae2510d827b1b
SeaCelo <SeaCelo@users.noreply.github.com>
2023-03-02 10:30:54 -0500

    Upload data
    Adding training data used to build the SDGClassy classifier. Also uploading the code and data used to build the combined model as described in this paper: https://ieeexplore.ieee.org/document/9730598

---
+++ training/UN-classifier/cl_sdg/joinfiles.sh
@@ -0,0 +1,19 @@
+cd /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/input && \
+cat sdg1-new.txt SDG_01-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg1-full.txt && \
+cat sdg2-new.txt SDG_02-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg2-full.txt && \
+cat sdg3-new.txt SDG_03-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg3-full.txt && \
+cat sdg4-new.txt SDG_04-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg4-full.txt && \
+cat sdg5-new.txt SDG_05-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg5-full.txt && \
+cat sdg6-new.txt SDG_06-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg6-full.txt && \
+cat sdg7-new.txt SDG_07-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg7-full.txt && \
+cat sdg8-new.txt SDG_08-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg8-full.txt && \
+cat sdg9-new.txt SDG_09-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg9-full.txt && \
+cat sdg10-new.txt SDG_10-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg10-full.txt && \
+cat sdg11-new.txt SDG_11-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg11-full.txt && \
+cat sdg12-new.txt SDG_12-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg12-full.txt && \
+cat sdg13-new.txt SDG_13-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg13-full.txt && \
+cat sdg14-new.txt SDG_14-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg14-full.txt && \
+cat sdg15-new.txt SDG_15-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg15-full.txt && \
+cat sdg16-new.txt SDG_16-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg16-full.txt && \
+cat sdg17-new.txt SDG_17-escap.txt > /Users/mlafleur/Desktop/themes/sdg_testing/classifier/cl_sdg/sdg17-full.txt && \
+echo "Command sequence finished successfully"
